620f0b48d8c03ba0ffbb4781b0bfa2e195c1c271
adityar15 <adityask2194@gmail.com>
2023-01-07 15:15:38 +0000

    netlify domain in socket

--- plugins/socketclient.ts
+++ plugins/socketclient.ts
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 export default defineNuxtPlugin(() => {
-    const socket = io('http://localhost:3001');
+    const socket = io('https://63b98bffcc26af2b65da6448--dreamy-taiyaki-856c76.netlify.app:3001');
 
     return {
         provide: {
