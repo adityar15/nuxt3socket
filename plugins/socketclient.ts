499c437e020379cf773a753bb45bcee67db3e29e
adityar15 <adityask2194@gmail.com>
2023-01-07 16:08:39 +0000

    socket .io path added

--- plugins/socketclient.ts
+++ plugins/socketclient.ts
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 export default defineNuxtPlugin(() => {
-    const socket = io('http://localhost:3001');
+    const socket = io('https://cool-snickerdoodle-547743.netlify.app:3001');
 
     return {
         provide: {
