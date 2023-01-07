6ef6f8584f31301c29be0b5d9b585b6e4c1e16ec
adityar15 <adityask2194@gmail.com>
2023-01-07 16:14:50 +0000

    socket message added

--- plugins/socketserver.server.ts
+++ plugins/socketserver.server.ts
@@ -19,6 +19,7 @@ export default defineNuxtPlugin(async (nitroApp) => {
         socket.emit("message", `Hello from server ${data}`)
       })
     })
+    console.log("socket server ready")
     globalThis.io = io
   }
 
